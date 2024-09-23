4ef3962b59afd4369eedce1ed577217988cfba74
JimmyN0912 <74365273+JimmyN0912@users.noreply.github.com>
2024-09-23 14:19:35 +0000

    V1.2 Changes: - Updated csv reading encoding to UTF-8 (Big5 -> UTF-8) - Restored correct subject text - Removed some unneeded comments

--- reminders.py
+++ reminders.py
@@ -11,7 +11,7 @@ webhook_url = os.getenv('MAKE_WEBHOOK_URL')
 # Dictionary mapping weekdays to subject names
 weekday_subjects = {
     0: '   機器人/生物 \n   機器人/生物 \n   國文 \n   數學 \n   英文作文 \n   英文作文 \n   自然充實 \n   數學',       # Monday
-    1: '   數學 \n   佑軒美人魚 \n   化學 \n   化學 \n   體育 \n   數學 \n   國文 \n   國文',    # Tuesday
+    1: '   數學 \n   物理 \n   化學 \n   化學 \n   體育 \n   數學 \n   國文 \n   國文',    # Tuesday
     2: '   國文 \n   全民國防教育 \n   英文 \n   化學 \n   班會 \n   團體活動 \n   地科 \n   化學',    # Wednesday
     3: '   家政 \n   家政 \n   本土語 \n   物理 \n   國文 \n   健康與護理 \n   數學 \n   英文',        # Thursday
     4: '   進階程設/生物 \n   進階程設/生物 \n   英文 \n   體育 \n   國文 \n   物理 \n   數學 \n   物理', # Friday
@@ -21,7 +21,7 @@ weekday_subjects = {
 
 def read_csv(file_path):
     events = []
-    with open(file_path, mode='r', encoding='Big5') as file:
+    with open(file_path, mode='r', encoding='UTF-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             event_name = row['Event name'].strip()
@@ -78,7 +78,7 @@ def check_events(csv_file_path):
     call_webhook(payload)
 
 def main():
-    csv_file_path = "C:\\Share\\Reminders.csv"  # Replace with your CSV file path
+    csv_file_path = "C:\\Share\\Reminders.csv"
     check_events(csv_file_path)
 
 if __name__ == "__main__":
