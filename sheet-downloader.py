212e243d2fcd5b7d6cbead461b0197482b9a7652
JimmyN0912 <74365273+JimmyN0912@users.noreply.github.com>
2024-10-10 20:06:27 +0800

    V1.9 Changes: - Edited sheet-downloader.py to use four spaces as indentation. - Fixed list index out or range error caused by events that has no end date.

--- sheet-downloader.py
+++ sheet-downloader.py
@@ -19,50 +19,51 @@ RANGE_NAME = "Sheet1!A4:E"
 
 
 def main():
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  try:
-    service = build("sheets", "v4", credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
-        .execute()
-    )
-    values = result.get("values", [])
-
-    if not values:
-      print("No data found.")
-      return
     try:
-      with open("C:\\Share\\Reminders.csv", mode="w", encoding="UTF-8", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Event name", "Event date and time", "Weekday"])
-        for row in values:
-          writer.writerow([row[0], row[3], row[4]])
-    except Exception as e:
-        print(e)
-  except HttpError as err:
-    print(err)
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+
+        if not values:
+            print("No data found.")
+            return
+        try:
+            with open("reminders.csv", mode="w", encoding="UTF-8", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(["Event name", "Event date and time", "Weekday"])
+                for row in values:
+                    if len(row) >= 5:
+                        writer.writerow([row[0], row[3], row[4]])
+        except Exception as e:
+            print(e)
+    except HttpError as err:
+        print(err)
 
 
 if __name__ == "__main__":
-  main()
+    main()
